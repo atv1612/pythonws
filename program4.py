69be6a989d8712e1c6570398f85b69b453ad2123
atv1612 <45307248+atv1612@users.noreply.github.com>
2019-06-20 14:32:21 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,9 @@
+def input1():
+    sname=input("enter your name")
+    dbname=input("enter the database name")
+    username=input("enter your username")
+    password=input("enter your passowrd")
+    return("Server name= %s\n  dbname= %s\n username=%s \n pssword =%s"%(sname,dbname,username,password))
+print(input1())
+    
+
